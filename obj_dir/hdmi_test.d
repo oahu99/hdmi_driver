5b9a05a9d7ed7297b5f5537f17c11622035afb72
John Hammond <john.hammond@posteo.net>
2019-08-16 23:00:35 -0500

    removed obj_dir

--- obj_dir/hdmi_test.d
+++
@@ -1,5 +0,0 @@
-hdmi_test.o: ../hdmi_test.cpp Vhdmi_driver.h \
- /usr/local/share/verilator/include/verilated.h \
- /usr/local/share/verilator/include/verilatedos.h \
- /usr/local/share/verilator/include/verilated_vcd_c.h \
- /usr/local/share/verilator/include/verilated.h
