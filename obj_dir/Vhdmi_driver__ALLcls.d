ad9c731b38517bbb80834b349fc041f4264e4f90
John Hammond <john.hammond@posteo.net>
2019-08-18 15:44:08 -0500

    removed obj_dir files

--- obj_dir/Vhdmi_driver__ALLcls.d
+++
@@ -1,3 +0,0 @@
-Vhdmi_driver__ALLcls.o: Vhdmi_driver__ALLcls.cpp Vhdmi_driver.cpp \
- Vhdmi_driver.h /usr/local/share/verilator/include/verilated.h \
- /usr/local/share/verilator/include/verilatedos.h Vhdmi_driver__Syms.h
--- obj_dir/hdmi_test_template.d
+++
@@ -1,3 +0,0 @@
-hdmi_test_template.o: ../hdmi_test_template.cpp Vhdmi_driver.h \
- /usr/local/share/verilator/include/verilated.h \
- /usr/local/share/verilator/include/verilatedos.h
